b7df89f840d0a9f9870df4818a5b45df234f606f
Kayy Still Learning <kenzonly12@gmail.com>
2026-09-24 21:50:27 +0700

    Remove fake default class/profile data

--- app/src/main/java/com/example/data/local/entity/UserSettingsEntity.kt
+++ app/src/main/java/com/example/data/local/entity/UserSettingsEntity.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 data class UserSettingsEntity(
     @PrimaryKey
     val id: Int = 1,
-    val userClass: String = "X IPA 2",
+    val userClass: String = "",
     val studentName: String = "",
     val schoolName: String = "",
     val autoFilterClassSchedule: Boolean = true
